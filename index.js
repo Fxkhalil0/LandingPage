4438ff8bf016931b7cd7a4883d12b086a020dc0c
Fxkhalil0 <fxkhalil0@gmail.com>
2023-12-30 23:50:13 +0200

    ...

--- index.js
+++ index.js
@@ -5,18 +5,16 @@ const server = require("http").createServer(app);
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('client/build'));
+
 app.use(cors());
-app.use(bodyParser.json());
 
 const userRoute = require("./Routers/userRoute");
 app.use("/user", userRoute);
 
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(express.static('client/build'));
 app.get("*", (req, res)=>{
   res.sendFile(`${__dirname}/client/build/index.html`)
 })
