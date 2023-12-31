26beaf3fa93c906b15d1473d14cce12beb5d7476
Fxkhalil0 <fxkhalil0@gmail.com>
2023-12-31 17:46:35 +0200

    ..

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://lvw:LVW123456789@cluster0.9bdwe65.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://lvw:LVW123456789@cluster0.9bdwe65.mongodb.net/Project?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
   console.log('Connected to MongoDB');
 })
