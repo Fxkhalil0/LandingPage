966a0fc198b85f9ca83ed09d0fc2beece9d1a578
Fxkhalil0 <fxkhalil0@gmail.com>
2023-12-31 15:10:26 +0200

    ..

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,10 @@
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_URI ,'mongodb+srv://lvw:LVW123456789@cluster0.9bdwe65.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => {
-  console.log('Connected to MongoDB');
-})
-.catch((error) => {
-  console.error('Error connecting to MongoDB', error);
-});
+const mongoose = require('mongoose');
+const connectionString = process.env.MONGODB_URI || 'mongodb+srv://lvw:LVW123456789@cluster0.9bdwe65.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB', error);
+  });
